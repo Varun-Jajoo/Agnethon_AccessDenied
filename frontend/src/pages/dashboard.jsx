55bc092cdc5d00e5661b76b60708aa552a38bd5d
Ritojnan <ritojnanm@gmail.com>
2024-02-25 11:45:24 +0530

    wrapper changed

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SEO from "../common/seo";
-import WrapperFour from "../layout/wrapper-4";
+import WrapperFour from "../layout/wrapper";
 import { useEffect, useState } from "react";
 import Dashboard from "../components/dashboard/dashboard";
 // import { ChakraProvider,extendTheme ,Box,} from "@chakra-ui/react";
